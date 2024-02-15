19eb7d9b5194314361f4afe04728499c9ea7bdb0
DarkHumor71 <128751050+DarkHumor71@users.noreply.github.com>
2024-02-15 17:01:36 +0200

    modified:    add long substring (not implemented)

--- Palindrome Checker/node.js
+++ Palindrome Checker/node.js
@@ -18,11 +18,28 @@ function palindrome(str) {
   let s = str.replace(/[^a-zA-Z0-9]/g, "");
   s = s.toLowerCase();
   s = s.trim();
-  console.log(s);
   let s1 = s.split("").reverse().join("");
-  console.log(s1);
-  if (s1 == s) return true;
-  else return false;
+  return s1 == s;
 }
+// function namer(str) {
+//   let q = str;
+//   for (let s = 0; s < q.length; s++) {
+//     for (let e = q.length; e > s; e--) {
+//       let a = q.slice(s, e);
+//       if (palindrome(a) && a.length > 2) {
+//         return q.slice(s, e);
+//       }
+//     }
+//   }
+// }
 
-palindrome("1 eye for of 1 eye.");
+// console.log(namer("aceexcivicgrfdds"));
+// console.log(namer("aceexcivicgrfdds"));
+// console.log(namer("civicgrfdds"));
+// console.log(namer("aceexcivic"));
+// console.log(namer("civic"));
+// console.log(namer("123abba1"));
+// console.log(namer("abba1"));
+// console.log(namer("123abba"));
+// console.log(namer("12345"));
+// console.log(namer(""));
